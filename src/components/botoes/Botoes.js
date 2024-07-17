754388f2a4c6205dfb1dd6a875af9ca3ac924bd2
Alan Manoel <alanmanoel517@gmail.com>
2024-07-17 16:59:44 -0300

    Mudando btn view para TouchableOpacity

--- src/components/botoes/Botoes.js
+++ src/components/botoes/Botoes.js
@@ -1,12 +1,15 @@
 import React from "react";
 import styles from './style'
 
-import { View, Text} from "react-native";
-export default function Botoes({ numero, cor="#464B53", colorText= "#FFF"}) {
+import { View, Text, TouchableOpacity } from "react-native";
+export default function Botoes({ numero, cor = "#464B53", colorText = "#FFF" }) {
   return (
-    <View style={[styles.button, { backgroundColor: cor, width: numero === "0" ? 176 : 84 }]}>
+    <TouchableOpacity
+      style={[styles.button, { backgroundColor: cor, width: numero === "0" ? 176 : 84 }]}
+    // onPress={onPress}
+    >
       <Text style={[styles.buttonText, { color: colorText }]}>{numero}</Text>
-    </View>
+    </TouchableOpacity>
   );
-}
+};
 
